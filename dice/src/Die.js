26383d006edaf5d9fc0e0fd870057b202ad4cdac
thermiee <navyblack16@yahoo.com>
2022-02-12 01:26:52 +0100

    Dice code

---
+++ dice/src/Die.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./Die.css"
+
+class Die extends Component {
+    render() {
+        let cls = `Die fas fa-dice-${this.props.face} ${this.props.rolling && 'shaking'}`
+        return <i className={cls} />
+    }
+}
+
+export default Die;
