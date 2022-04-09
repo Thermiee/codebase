e674e790d5820bb65a04280cdb71671cc7df3d1a
thermiee <navyblack16@yahoo.com>
2022-04-09 22:06:21 +0100

    add react project score keeper

---
+++ scorekeeper/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import ScoreKeeper from './ScoreKeeper';
+import IconList from './IconList';
+
+function App() {
+  return (
+    <div className="App">
+      <ScoreKeeper />
+      <IconList />
+    </div>
+  );
+}
+
+export default App;
