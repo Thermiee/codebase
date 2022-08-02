656fb6d774697a7cc395c3e1548efeeb3eb97a52
thermiee <navyblack16@yahoo.com>
2022-08-02 23:42:10 +0100

    Code for shopping list

---
+++ shopping-list-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import ShoppingList from './ShoppingList';
+
+function App() {
+  return (
+    <div className="App">
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
