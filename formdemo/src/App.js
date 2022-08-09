93ccd67c385d126e3915225dee5a615f56d8b2bd
thermiee <navyblack16@yahoo.com>
2022-08-09 16:58:09 +0100

    Code for form demo

---
+++ formdemo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+import MultipleForms from './MultipleForms';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <MultipleForms />
+    </div>
+  );
+}
+
+export default App;
